c97d56ae90fa76270312d6eb827e9b4edcb712df
Arjun Bhatia <bhatia.arj@northeastern.edu>
2023-02-01 18:43:24 -0500

    unitcommit

---
+++ unitestuser.py
@@ -0,0 +1,20 @@
+import unittest
+import os
+# from os import path
+
+class BasicTest(unittest.TestCase):
+
+    def test(self):
+
+        if(os.path.exists('user.py')):
+            self.assertTrue(True)
+        else:
+            self.assertTrue(False)
+
+
+
+
+if __name__ == '__main__':
+
+        unittest.main()
+
