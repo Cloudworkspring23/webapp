7e32e855175f64965fa02159d21043ec39bb6d29
Arjun Bhatia <bhatia.arj@northeastern.edu>
2023-02-23 00:04:36 -0500

    last_push

--- database.py
+++ database.py
@@ -44,7 +44,7 @@ def createTable_product():
     database="clouddb"
     )
     mycursor = mydb.cursor()
-    mycursor.execute("CREATE TABLE if not exists tbl_product(p_id bigint Auto_Increment Primary Key,P_name varchar(45) DEFAULT NULL,description varchar(100) DEFAULT NULL,sku varchar(10) Unique,Manufacturer varchar(20) DEFAULT NULL,Quantity bigint,Date_added varchar(50) Default Null,Date_last_update varchar(50),u_id bigint, foreign key(u_id) references tbl_Create_User(u_id) )ENGINE=InnoDB AUTO_INCREMENT=1")
+    mycursor.execute("CREATE TABLE if not exists tbl_product(p_id bigint Auto_Increment Primary Key,P_name varchar(45) DEFAULT NULL,description varchar(100) DEFAULT NULL,sku varchar(10) Unique,Manufacturer varchar(20) DEFAULT NULL,Quantity bigint,Date_added varchar(50) Default Null,Date_last_update varchar(50),u_id bigint, foreign key(u_id) references tbl_create_user(u_id) )ENGINE=InnoDB AUTO_INCREMENT=1")
     mycursor.close()
 
 
